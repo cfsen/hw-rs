42b7180c4b29095a17ab46b32e124b456b8bc1d1
cfsen <103598332+cfsen@users.noreply.github.com>
2026-03-28 11:00:19 +0100

    add: object decoding detour in HWMessage

--- src/hyprwire/message.rs
+++ src/hyprwire/message.rs
@@ -2,7 +2,7 @@
 
 use std::{io::Read, os::unix::net::UnixStream};
 
-use crate::hyprwire::{error::HyprwireError, payload::HWPayload, types::{HWMagic, HWMessageKind, HWValue}};
+use crate::hyprwire::{error::HyprwireError, payload::HWPayload, types::{HWMagic, HWMessageKind, HWValue}, vlq::VLQ};
 
 
 pub struct HWMessage {
@@ -53,6 +53,50 @@ impl HWMessage {
         }
     }
 }
+/// object decoding detour
+impl HWMessage {
+    pub fn decode_object(bin: &[u8]) -> Result<Vec<HWPayload>, HyprwireError> {
+        let mut payload = Vec::new();
+        let mut cursor = 0;
+
+        while cursor < bin.len() {
+            let magic = HWMagic::try_from(&bin.get(cursor))?;
+            match magic {
+                HWMagic::End => break,
+                _ => { },
+            }
+            let offset;
+            cursor += 1;
+
+            match magic {
+                HWMagic::Varchar => {
+                    let Some((data_len, vlq_offset)) = VLQ::decode(&bin[cursor..]) 
+                    else { return Err(HyprwireError::WIP); };
+
+                    let value = HWValue::from_slice(magic, &bin[cursor+vlq_offset..cursor+vlq_offset+data_len as usize])
+                        .map_err(|_| HyprwireError::WIP)?; // TODO: bubble error
+
+                    payload.push(HWPayload { magic, value });
+                    offset = vlq_offset + data_len as usize;
+                },
+                _ => {
+                    let bytes: [u8; 4] = bin[cursor..cursor+4]
+                        .try_into()
+                        .map_err(|_| HyprwireError::WIP)?; // TODO: bubble error
+
+                    let value = HWValue::from_slice(magic, &bytes)?;
+
+                    payload.push(HWPayload { magic, value });
+                    offset = 4;
+                },
+            }
+
+            cursor += offset;
+        }
+
+        Ok(payload)
+    }
+}
 impl TryFrom<&mut UnixStream> for HWMessage {
     type Error = HyprwireError;
 
@@ -61,13 +105,38 @@ impl TryFrom<&mut UnixStream> for HWMessage {
         let n = stream.read(&mut buf)
             .map_err(|e| HyprwireError::MessageStreamRead(e))?;
 
+        println!("-> rx: {:?}", &buf[..n]);
 
         let kind = HWMessageKind::try_from(&buf.get(0))?;
-        let magic = HWMagic::try_from(&buf.get(1))?;
 
-        let val = HWValue::from_slice(magic, &buf[2..n])?;
+        let payload = match kind {
+            HWMessageKind::NewObject => {
+                Self::decode_object(&buf[1..n])?
+            },
+            HWMessageKind::Generic => {
+                if n <= 6 {
+                    return Err(HyprwireError::WIP);
+                }
 
-        let payload = vec![HWPayload::from_magic_value(magic, val)];
+                // NOTE: first four bytes of generic messages contain object id
+                let mut result = Vec::new();
+                let object_id = HWPayload { 
+                    magic: HWMagic::ObjectId,
+                    value: HWValue::from_slice(HWMagic::ObjectId, &buf[2..6])?,
+                };
+                let decode = Self::decode_object(&buf[6..n])?;
+
+                result.push(object_id);
+                result.extend(decode);
+                result
+            },
+            _ => {
+                let magic = HWMagic::try_from(&buf.get(1))?;
+                let val = HWValue::from_slice(magic, &buf[2..n])?;
+
+                vec![HWPayload::from_magic_value(magic, val)]
+            }
+        };
 
         Ok(Self { kind, payload })
     }
